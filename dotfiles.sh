d3c7660d9b1ce3de7386b27f07ada9cb2169b72a
Harish Chandar <149600195+Harish-Chandar@users.noreply.github.com>
2026-07-24 16:48:10 -0500

    check if stow is installed

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,6 +8,23 @@ packages=(
 
 # stow dotfiles
 
+if ! command -v stow &> /dev/null; then
+	echo "gnu-stow could not be found. Would you like to install it? (y/n)"
+	read -r answer
+	if [[ "$answer" == "y" ]]; then
+		if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+			sudo apt-get update
+			sudo apt-get install stow
+		else
+			echo "Unsupported OS package manager. Please install stow manually."
+			exit 1
+		fi
+	else
+		echo "gnu-stow is required to run this script. Install manually and run this file."
+		exit 1
+	fi
+fi
+
 for package in "${packages[@]}"; do
 	stow -v -t ~ "$package"
 done
